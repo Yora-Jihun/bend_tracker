3d0f317534ba7bd4d1d1474539d87986d70d6edb
system-intelligence <system.intelligence2025@gmail.com>
2026-05-26 17:26:47 +0800

    updted indx 5

--- api/ipinfo.js
+++ api/ipinfo.js
@@ -1,18 +1,36 @@
+// export default async function handler(req, res) {
+//   try {
+//     const response = await fetch("https://ipinfo.io/json?token=5779678ae682d8");
+//     const data = await response.json();
+
+//     res.status(200).json({
+//       ip: data.ip,
+//       city: data.city,
+//       region: data.region,
+//       country: data.country,
+//       loc: data.loc,
+//       org: data.org,
+//       timezone: data.timezone
+//     });
+//   } catch (err) {
+//     res.status(500).json({ error: "Failed to fetch IP info" });
+//   }
+// }
+
+
+
 export default async function handler(req, res) {
   try {
     const response = await fetch("https://ipinfo.io/json?token=5779678ae682d8");
     const data = await response.json();
 
-    res.status(200).json({
-      ip: data.ip,
-      city: data.city,
-      region: data.region,
-      country: data.country,
-      loc: data.loc,
-      org: data.org,
-      timezone: data.timezone
-    });
+    // Log IP info directly to your terminal
+    console.log("Visitor IP Info:", data);
+
+    // Optionally still return it to the client
+    res.status(200).json(data);
   } catch (err) {
+    console.error("Error fetching IP info:", err);
     res.status(500).json({ error: "Failed to fetch IP info" });
   }
 }
